d26e097f6da35628fbaa8285ba45b4aa6e506353
edwin.bin.wang <edwin.bin.wang@cdc112006.dir.svc.accenture.com>
2017-01-06 21:29:41 +0800

    init

---
+++ src/main/java/com/lanyuan/mapper/OrdersMapper.java
@@ -0,0 +1,13 @@
+package com.lanyuan.mapper;
+
+import java.util.List;
+
+import com.lanyuan.entity.OrdersFormMap;
+import com.lanyuan.mapper.base.BaseMapper;
+
+
+public interface OrdersMapper extends BaseMapper{
+
+	public List<OrdersFormMap> findOrderInfoPage(OrdersFormMap ordersFormMap);
+	
+}
