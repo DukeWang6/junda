f00fd9770281e17edb439cd88dee7c00186e0151
edwin.bin.wang <edwin.bin.wang@cdc112006.dir.svc.accenture.com>
2017-01-07 16:55:22 +0800

    1

---
+++ src/main/webapp/WEB-INF/jsp/system/smallexp/list.jsp
@@ -0,0 +1,62 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>	
+<script type="text/javascript" src="${pageContext.request.contextPath}/js/system/smallexp/list.js"></script>
+<link rel="stylesheet" type="text/css" media="all" href="js/date/daterangepicker-bs3.css" />
+<script type="text/javascript" src="js/date/moment.js"></script>
+<script type="text/javascript" src="js/date/daterangepicker.js"></script>
+	<div class="m-b-md">
+		<form class="form-inline" role="form" id="searchForm"
+			name="searchForm">
+			<div class="form-group">
+				<label class="control-label"> <span
+					class="h4 font-thin v-middle">乘客电话:</span></label> <input
+					class="input-medium ui-autocomplete-input" id="userTel"
+					name="SmallExpFormMap.userTel">
+			</div>
+			<div class="form-group">
+				<label class="control-label"> <span
+					class="h4 font-thin v-middle">司机电话:</span></label> <input
+					class="input-medium ui-autocomplete-input" id="driverTel" 
+					name="SmallExpFormMap.driverTel">
+			</div>
+			
+			<div class="form-group">
+				<label class="control-label"> <span
+					class="h4 font-thin v-middle">订单状态:</span></label> <input
+					class="input-medium ui-autocomplete-input" id="ordersState"
+					name="SmallExpFormMap.ordersState">
+			</div>
+			
+			<a href="javascript:void(0)" class="btn btn-default" id="search">查询</a>
+		</form>
+	</div>
+	<header class="panel-heading">
+	<div class="doc-buttons">
+		<c:forEach items="${res}" var="key">
+			${key.description}
+		</c:forEach>
+	</div>
+	</header>
+	<div class="table-responsive">
+		<div id="paging" class="pagclass"></div>
+	</div>
+	
+<script type="text/javascript">
+	$(document).ready(function() {
+	   $('#incarDate').daterangepicker({
+	     timePicker: true,
+	     timePickerIncrement: 30,
+	     format: 'YYYY-MM-DD HH:mm'
+	   }, function(start, end, label) {
+	     console.log(start.toISOString(), end.toISOString(), label);
+	   });
+	   $('#outcarDate').daterangepicker({
+		     timePicker: true,
+		     timePickerIncrement: 30,
+		     format: 'YYYY-MM-DD HH:mm'
+		   }, function(start, end, label) {
+		     console.log(start.toISOString(), end.toISOString(), label);
+		   });
+	});
+</script>
---
+++ src/main/webapp/js/system/smallexp/list.js
@@ -0,0 +1,183 @@
+var pageii = null;
+var grid = null;
+$(function() {
+	
+	grid = lyGrid({
+		pagId : 'paging',
+		l_column : [{
+			colkey : "Location",
+			name : "起始地址",
+			isSort:true
+		}, {
+			colkey : "Destination",
+			name : "目的地地址",
+			isSort:true
+		}, {
+			colkey : "OrdersState",
+			name : "订单状态",
+			isSort:true
+		}, {
+			colkey : "Subsidy",
+			name : "补偿费",
+			isSort:true
+		},  {
+			colkey : "ActualPrice",
+			name : "订单金额",
+			isSort:true
+		}, {
+			colkey : "PhoneNumber",
+			name : "乘客电话",
+			isSort:true
+		}, {
+			colkey : "ID",
+			name : "订单ID",
+			isSort:true
+		}{
+			colkey : "DriverTelephone",
+			name : "司机电话",
+			isSort:true
+		},{
+			colkey : "CancelOrders",
+			name : "取消原因",
+			isSort:true
+		},{
+			colkey : "GrabDate",
+			name : "抢单时间",
+			isSort:true,
+			renderData : function(rowindex,data, rowdata, column) {
+				if(data==null||data=="") {
+					return null;
+				} else {
+					return new Date(data).format("yyyy-MM-dd hh:mm:ss");
+				}
+			}
+		},{
+			colkey : "PickupTime",
+			name : "取件时间",
+			isSort:true,
+			renderData : function(rowindex,data, rowdata, column) {
+				if(data==null||data=="") {
+					return null;
+				} else {
+					return new Date(data).format("yyyy-MM-dd hh:mm:ss");
+				}
+			}
+		},{
+			colkey : "ArrivedTime",
+			name : "到达时间",
+			isSort:true,
+			renderData : function(rowindex,data, rowdata, column) {
+				if(data==null||data=="") {
+					return null;
+				} else {
+					return new Date(data).format("yyyy-MM-dd hh:mm:ss");
+				}
+			}
+		},{
+			colkey : "ExpressName",
+			name : "快递名称",
+			isSort:true,
+		},{
+			colkey : "GoodsWithOwner",
+			name : "是否押运同行",
+			isSort:true,
+		},{
+			colkey : "IsReceiptedByOthor",
+			name : "是否自己接货",
+			isSort:true,
+		},{
+			colkey : "ReceiptName",
+			name : "接货人",
+			isSort:true,
+		},{
+			colkey : "ReceiptTel",
+			name : "接货人电话",
+			isSort:true,
+		},{
+			colkey : "IsArrivalOnTime",
+			name : "是否按时送达",
+			isSort:true,
+		}],
+		jsonUrl : rootPath + '/smallExp/findByPage.shtml',
+		dymCol:true,
+		checkbox : true,
+		serNumber : true
+	});
+	$("#search").click("click", function() {// 绑定查询按扭
+		var searchParams = $("#searchForm").serializeJson();// 初始化传参数
+		grid.setOptions({
+			data : searchParams
+		});
+	});
+});
+	
+
+$("#addVersioninfo").click("click", function() {
+	addVersioninfo();
+});
+$("#editVersioninfo").click("click", function() {
+	editVersioninfo();
+});
+$("#delVersioninfo").click("click", function() {
+	delVersioninfo();
+});
+
+function addVersioninfo() {
+	pageii = layer.open({
+		title : "新增",
+		type : 2,
+		area : [ "600px", "80%" ],
+		content : rootPath + '/versioninfo/addUI.shtml'
+	});
+}
+
+function editVersioninfo() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox.length > 1) {
+		layer.msg("只能选中一个");
+		return;
+	}
+	if (cbox == "") {
+		layer.msg("必须选中一个");
+		return;
+	}
+	pageii = layer.open({
+		title : "编辑",
+		type : 2,
+		area : [ "600px", "80%" ],
+		content : rootPath + '/versioninfo/editUI.shtml?id=' + cbox
+	});
+}
+function delVersioninfo() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox == "") {
+		layer.msg("请选择删除项！！");
+		return;
+	}
+	layer.confirm('是否删除？', function(index) {
+		var url = rootPath + '/versioninfo/deleteEntity.shtml';
+		var s = CommnUtil.ajax(url, {
+			ids : cbox.join(",")
+		}, "json");
+		if (s == "success") {
+			layer.msg('删除成功');
+			grid.loadData();
+		} else {
+			layer.msg('删除失败');
+		}
+	});
+}
+function permissions() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox.length > 1 || cbox == "") {
+		layer.msg("请选择一个对象！");
+		return;
+	}
+	var url = rootPath + '/resources/permissions.shtml?userId='+cbox;
+	pageii = layer.open({
+		title : "分配权限",
+		type : 2,
+		area : [ "700px", "80%" ],
+		content : url
+	});
+}
